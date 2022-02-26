a7883e022195499224dec027c93a2b4846e3a95d
Eric Hauser <ewhauser@gmail.com>
2022-02-26 11:53:07 -0700

    initial import

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE}")";
+
+git pull origin main;
+
+rsync --exclude ".git/" \
+    --exclude ".DS_Store" \
+    --exclude ".osx" \
+    --exclude "bootstrap.sh" \
+    --exclude "README.md" \
+    --exclude "LICENSE-MIT.txt" \
+    -avh --no-perms . ~;
+    source ~/.zshrc;
